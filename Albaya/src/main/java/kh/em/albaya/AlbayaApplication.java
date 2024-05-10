df3c1df7f4fdd255332c3580573e8b4ede55e808
InkyungLee <yklee980216@naver.com>
2024-05-10 16:39:35 +0900

    no message

---
+++ Albaya/src/main/java/kh/em/albaya/AlbayaApplication.java
@@ -0,0 +1,13 @@
+package kh.em.albaya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbayaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlbayaApplication.class, args);
+	}
+
+}
---
+++ Albaya/src/test/java/kh/em/albaya/AlbayaApplicationTests.java
@@ -0,0 +1,13 @@
+package kh.em.albaya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlbayaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
