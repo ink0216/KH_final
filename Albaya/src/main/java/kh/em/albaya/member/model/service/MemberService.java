194841df7e0ab796e04b85030638631f1ad6ae0f
JoNee <hhcho1234@naver.com>
2024-05-16 11:39:15 +0900

    회원가입 페이지 합치기

---
+++ Albaya/src/main/java/kh/em/albaya/member/model/service/MemberService.java
@@ -0,0 +1,13 @@
+package kh.em.albaya.member.model.service;
+
+import kh.em.albaya.member.model.dto.Member;
+
+public interface MemberService {
+
+	/** 회원가입
+	 * @param member
+	 * @return result
+	 */
+	int signup(Member member);
+
+}
---
+++ Albaya/src/main/java/kh/em/albaya/member/model/service/MemberServiceImpl.java
@@ -0,0 +1,29 @@
+package kh.em.albaya.member.model.service;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import kh.em.albaya.member.model.dto.Member;
+import kh.em.albaya.member.model.mapper.MemberMapper;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class MemberServiceImpl implements MemberService{
+	
+	private final MemberMapper mapper;
+	
+	private final BCryptPasswordEncoder bcrypt;
+	
+	@Override
+	public int signup(Member member) {
+		String encPw = bcrypt.encode(member.getMemberPw());
+		member.setMemberEmail(member.getMemberEmail());
+		member.setMemberPhoneNumber(member.getMemberPhoneNumber());
+		member.setMemberPw(encPw);
+		member.setMemberName(member.getMemberName());
+		member.setMemberAddress(member.getMemberAddress());
+		member.setMemberGender(member.getMemberGender());
+		return mapper.signup(member);
+	}
+}
