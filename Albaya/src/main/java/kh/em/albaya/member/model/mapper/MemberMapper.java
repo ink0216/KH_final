0e559bfe0ebc8aee425b3ead6777180d38d4434d
InkyungLee <yklee980216@naver.com>
2024-05-16 14:14:05 +0900

    no message

--- Albaya/src/main/java/kh/em/albaya/member/model/mapper/MemberMapper.java
+++ Albaya/src/main/java/kh/em/albaya/member/model/mapper/MemberMapper.java
@@ -1,7 +1,9 @@
 package kh.em.albaya.member.model.mapper;
 
-import kh.em.albaya.member.model.dto.Member;
+import org.apache.ibatis.annotations.Mapper;
 
+import kh.em.albaya.member.model.dto.Member;
+@Mapper
 public interface MemberMapper {
 
 	/** 회원가입
