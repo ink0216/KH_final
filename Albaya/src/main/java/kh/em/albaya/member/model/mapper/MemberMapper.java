7bc85c533d866c12b2ed376b062719e68642f9e5
NTH <tang0929@naver.com>
2024-05-16 14:23:31 +0900

    노태형 : 게시판 기본 틀 진행중

--- Albaya/src/main/java/kh/em/albaya/member/model/mapper/MemberMapper.java
+++ Albaya/src/main/java/kh/em/albaya/member/model/mapper/MemberMapper.java
@@ -1,7 +1,10 @@
 package kh.em.albaya.member.model.mapper;
 
+import org.apache.ibatis.annotations.Mapper;
+
 import kh.em.albaya.member.model.dto.Member;
 
+@Mapper
 public interface MemberMapper {
 
 	/** 회원가입
