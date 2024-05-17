c84a2ad5ad0661a9d6a29fc59eac10c395ce4634
OZ <macoto8@naver.com>
2024-05-17 09:41:26 +0900

    board-lej

--- Albaya/src/main/java/kh/em/albaya/board/controller/EditBoardController.java
+++ Albaya/src/main/java/kh/em/albaya/board/controller/EditBoardController.java
@@ -1,8 +1,17 @@
 package kh.em.albaya.board.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class EditBoardController {
+	
+	// 게시글 작성화면 전환
+	@GetMapping()
+	public String boardInsert() {
+		
+		return "board/boardWrite";
+	}
+	
 
 }
