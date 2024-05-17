023507260766b052d6c3aa23bc4240364a2c1ce5
InkyungLee <yklee980216@naver.com>
2024-05-17 14:41:05 +0900

    LIK

--- Albaya/src/main/java/kh/em/albaya/board/controller/EditBoardController.java
+++ Albaya/src/main/java/kh/em/albaya/board/controller/EditBoardController.java
@@ -6,12 +6,5 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class EditBoardController {
 	
-	// 게시글 작성화면 전환
-	@GetMapping()
-	public String boardInsert() {
-		
-		return "board/boardWrite";
-	}
 	
-
 }
