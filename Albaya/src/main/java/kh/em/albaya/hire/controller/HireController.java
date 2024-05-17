c2fb41e36a5090fec36bf0b841829130dbccfcc0
jkh <jkhwang58@gmail.com>
2024-05-17 12:55:46 +0900

    유효성검사 거의 완성

--- Albaya/src/main/java/kh/em/albaya/hire/controller/HireController.java
+++ Albaya/src/main/java/kh/em/albaya/hire/controller/HireController.java
@@ -1,8 +1,14 @@
 package kh.em.albaya.hire.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("hire")
 public class HireController {
-
+   @GetMapping("hireWrite")
+   public String hireWrite() {
+      return "/hire/hireWrite";
+   }
 }
