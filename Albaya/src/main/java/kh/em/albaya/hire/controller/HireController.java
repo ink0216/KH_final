4d9d39da4389bc9ae7f720a829068862461baa35
InkyungLee <yklee980216@naver.com>
2024-05-20 16:14:27 +0900

    LIK

--- Albaya/src/main/java/kh/em/albaya/hire/controller/HireController.java
+++ Albaya/src/main/java/kh/em/albaya/hire/controller/HireController.java
@@ -1,16 +1,22 @@
 package kh.em.albaya.hire.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import kh.em.albaya.hire.model.dto.Hire;
+import kh.em.albaya.hire.model.service.HireService;
+import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequestMapping("hire")
+@RequiredArgsConstructor
 public class HireController {
-
+	private final HireService service;
+	
 	/**공고 등록 페이지로 이동
 	 * @return
 	 */
@@ -24,8 +30,25 @@ public class HireController {
 	 */
 	@PostMapping("hireWrite")
 	public String hireWrite(
-			Hire hire
+			Hire hire,
+			Model model
 			) {
+		/*hireNo
+		 * typeNo --직종 번호
+		 * payNo --급여 종류
+		 * hireTitle
+		 * hireContent
+		 * hireStart
+		 * hireEnd
+		 * hireCount
+		 * hireTerm -- 근무 기간
+		 * hireGender
+		 * applyCount
+		 * hireStatus
+		 * 
+		 * --아직 없는 네 개!!
+		 * hireGender
+		 * */
 		return "redirect:/"; 
 		//일단 메인페이지로 리다이렉트(나중에 작성된 공고 상세조회 페이지로 변경예정)
 	}
--- Albaya/src/main/java/kh/em/albaya/hire/model/dto/Hire.java
+++ Albaya/src/main/java/kh/em/albaya/hire/model/dto/Hire.java
@@ -18,16 +18,34 @@ public class Hire {
 	//HIRE 테이블 컬럼
 	private int hireNo; //공고문 번호
 	private int shopNo; //상점 번호
-	private int typeNo; //직종 번호
+	private String typeName; //직종 이름
 	private String hireContent; //주요 사업 내용
 	private String hireEnd; //모집 마감 날짜
 	private int hireTerm; //근무 기간(일 단위)
 	private int hirePay; //급여(월 단위)
-	private String hireDay; //근무 요일
+	private String workDay; //근무 요일
 	// 월화수목금토일 name값 같은 checkbox 사용하면 -> String으로 넘어옴('월,화') 이런 식으로
 	//	-> 화면에 뿌리거나 할 때에는 ,를 기준으로 split해서 쪼개면 된다
 	private int hireCount; //모집인원
 	private String hireStart; //모집 시작 날짜
 	private String hireGender; //모집 성별
-
+	/*hireNo==
+	 * typeName 직종 이름
+	 * payNo --급여 종류
+	 * payInput
+	 * hireTitle
+	 * hireContent
+	 * hireEnd
+	 * hireCount
+	 * address
+	 * addressDetail
+	 * shopName 회사명
+	 * hireTerm -- 근무 기간
+	 * hireGender==
+	 * applyCount==
+	 * workStart
+	 * workEnd
+	 * hireStatus==
+	 * workDay
+	 * */
 }
