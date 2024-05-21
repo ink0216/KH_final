c68b17a37c1dcd9ba869e9d331e67074358fecb5
InkyungLee <yklee980216@naver.com>
2024-05-21 16:02:03 +0900

    no message

--- Albaya/src/main/java/kh/em/albaya/hire/controller/HireController.java
+++ Albaya/src/main/java/kh/em/albaya/hire/controller/HireController.java
@@ -5,10 +5,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import kh.em.albaya.hire.model.dto.Hire;
 import kh.em.albaya.hire.model.service.HireService;
+import kh.em.albaya.member.model.dto.Member;
 import lombok.RequiredArgsConstructor;
 
 @Controller
@@ -31,26 +33,26 @@ public class HireController {
 	@PostMapping("hireWrite")
 	public String hireWrite(
 			Hire hire,
-			Model model
-			) {
-		/*hireNo
-		 * typeNo --직종 번호
-		 * payNo --급여 종류
-		 * hireTitle
-		 * hireContent
-		 * hireStart
-		 * hireEnd
-		 * hireCount
-		 * hireTerm -- 근무 기간
-		 * hireGender
-		 * applyCount
-		 * hireStatus
-		 * 
-		 * --아직 없는 네 개!!
-		 * hireGender
-		 * */
-		return "redirect:/"; 
-		//일단 메인페이지로 리다이렉트(나중에 작성된 공고 상세조회 페이지로 변경예정)
+			Model model,
+			@SessionAttribute("loginMember") Member loginMember,
+			RedirectAttributes ra
+			) { //SEQUENCE : 공고 번호 시퀀스만 생성했다
+		//HIRE 테이블에 INSERT하는 서비스 호출
+		int hireNo = service.hireWrite(hire);
+		String message=null;
+		String path = null;
+		if(hireNo>0) {
+			//공고 등록 성공 시
+			message="공고가 성공적으로 등록되었습니다.";
+			path="/hire/"+hireNo; //등록 성공한 공고 상세조회 페이지로 
+		}else {
+			
+			//공고 등록 실패 시
+			message="공고 등록에 실패하였습니다.";
+			path = "hireWrite";
+		}
+		ra.addFlashAttribute("message", message);
+		return "redirect:"+path; 
 	}
 	
 	@GetMapping("hirePopup")
--- Albaya/src/main/java/kh/em/albaya/hire/model/service/HireServiceImpl.java
+++ Albaya/src/main/java/kh/em/albaya/hire/model/service/HireServiceImpl.java
@@ -1,8 +1,28 @@
 package kh.em.albaya.hire.model.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import kh.em.albaya.hire.model.dto.Hire;
+import kh.em.albaya.hire.model.mapper.HireMapper;
+import lombok.RequiredArgsConstructor;
+@Transactional
 @Service
+@RequiredArgsConstructor
 public class HireServiceImpl implements HireService{
-
+	private final HireMapper mapper;
+	
+	//공고 등록
+	@Override
+	public int hireWrite(Hire hire) {
+		
+		//HIRE테이블에 INSERT하는 매퍼
+		int result = mapper.hireInsert(hire);
+		if(result ==0) {
+			return 0;
+		}
+		int hireNo = hire.getHireNo(); //삽입 성공한 공고 번호
+		
+		return 0;
+	}
 }
