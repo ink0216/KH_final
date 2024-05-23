98ca80407817434d20a072a891bd29caeb253c3e
OZ <macoto8@naver.com>
2024-05-23 16:29:06 +0900

    토크게시판 삭제

--- Albaya/src/main/java/kh/em/albaya/board/controller/EditBoardController.java
+++ Albaya/src/main/java/kh/em/albaya/board/controller/EditBoardController.java
@@ -1,11 +1,18 @@
 package kh.em.albaya.board.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import kh.em.albaya.board.model.service.EditBoardService;
+import kh.em.albaya.member.model.dto.Member;
 import lombok.RequiredArgsConstructor;
 
 @Controller
@@ -18,7 +25,7 @@ public class EditBoardController {
 	
 	// 게시글 작성화면 전환
 	@GetMapping("{reviewBoardCode:[12]}/insert")
-	public String boardInsert(
+	public String reviewBoardInsert(
 		@PathVariable("reviewBoardCode") int reviewBoardCode) {
 		
 		return "reviewBoard/reviewBoardWrite";
@@ -29,6 +36,37 @@ public class EditBoardController {
 	
 	
 	// 게시글 삭제
+	@GetMapping("{reviewBoardCode:[12]}/{reviewBoardNo:[0-9]+}/delete")
+	public String reviewBoardDelete(
+		@PathVariable("reviewBoardCode") int reviewBoardCode,
+		@PathVariable("reviewBoardNo") int reviewBoardNo,
+		@SessionAttribute("loginMember") Member loginMember,
+		@RequestParam(value="cp", required=false, defaultValue = "1")int cp,
+		RedirectAttributes ra) {
+		
+		Map<String, Integer> map = new HashMap<>();
+		map.put("reviewBoardCode", reviewBoardCode);
+		map.put("reviewBoardNo", reviewBoardNo);
+		map.put("memberNo", loginMember.getMemberNo());
+		
+		int result = service.reviewBoardDelete(map);
+		
+		String path = null;
+		String message = null;
+		
+		
+		if(result>0) {
+			path = String.format("/reviewBoard/%d",reviewBoardCode);
+			message = "삭제 완료되었습니다";
+		}else {
+			path = String.format("/reviewBoard/%d/%d/%d", reviewBoardCode, reviewBoardNo,cp);
+			message = "삭제 실패하였습니다";
+		}
+		
+		ra.addFlashAttribute("message",message);
+		
+		return "redirect:" + path;
+	}
 	
 	
 
--- Albaya/src/main/java/kh/em/albaya/board/model/service/EditBoardServiceImpl.java
+++ Albaya/src/main/java/kh/em/albaya/board/model/service/EditBoardServiceImpl.java
@@ -1,8 +1,23 @@
 package kh.em.albaya.board.model.service;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Service;
 
+import kh.em.albaya.board.model.mapper.EditBoardMapper;
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class EditBoardServiceImpl implements EditBoardService {
+	
+	private final EditBoardMapper mapper;
+	
+	
+	// 게시글 삭제
+	@Override
+	public int reviewBoardDelete(Map<String, Integer> map) {
+		return mapper.reviewBoardDelete(map) ;
+	}
 
 }
