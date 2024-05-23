de42e2ff330f05bebcbb6798d6ca1507beb19e53
InkyungLee <yklee980216@naver.com>
2024-05-23 17:43:07 +0900

    시군구동 데이터 DB 삽입 완료

--- Albaya/src/main/java/kh/em/albaya/location/mapper/LocationMapper.java
+++ Albaya/src/main/java/kh/em/albaya/location/mapper/LocationMapper.java
@@ -2,7 +2,29 @@ package kh.em.albaya.location.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import kh.em.albaya.location.dto.Dong;
+import kh.em.albaya.location.dto.Dosi;
+import kh.em.albaya.location.dto.Sigungu;
+
 @Mapper
 public interface LocationMapper {
+
+	/**도/시 insert
+	 * @param dosi
+	 * @return
+	 */
+	int insertDosi(Dosi dosi);
+
+	/**시/군/구 insert
+	 * @param sigungu
+	 * @return
+	 */
+	int insertSigungu(Sigungu sigungu);
+
+	/**동 insert
+	 * @param dong
+	 * @return
+	 */
+	int insertDong(Dong dong);
 
 }
--- Albaya/src/main/java/kh/em/albaya/location/service/LocationServiceImpl.java
+++ Albaya/src/main/java/kh/em/albaya/location/service/LocationServiceImpl.java
@@ -1,5 +1,6 @@
 package kh.em.albaya.location.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -20,10 +21,36 @@ public class LocationServiceImpl implements LocationService{
 	//위치 데이터 모두 db에 insert
 	@Override
 	public int insertDb(List<Dosi> dosiList, List<Sigungu> sigunguList, List<Dong> dongList) {
+//		// 도/시 삽입
+//		for(int i=0; i<dosiList.size(); i++) {
+//			Dosi dosi = dosiList.get(i);
+//			int result = mapper.insertDosi(dosi);
+//			if(result==0) {
+//				throw new RuntimeException("도/시 삽입 실패");
+//			}
+//		}
+		
+//		// 시/군/구 삽입
+//		for(int i=0; i<sigunguList.size(); i++) {
+//			Sigungu sigungu = sigunguList.get(i);
+//			int result = mapper.insertSigungu(sigungu);
+//			if(result==0) {
+//				throw new RuntimeException("시/군/구 삽입 실패");
+//			}
+//		}
+		
+		// 동 삽입
+		for(int i=0; i<dongList.size(); i++) {
+			Dong dong = dongList.get(i);
+			int result = mapper.insertDong(dong);
+			if(result==0) {
+				throw new RuntimeException("동 삽입 실패");
+			}
+		}
 		
 		
 		
-		return 0;
+		return 1;
 	}
 	
 	
