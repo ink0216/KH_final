c9c515484d51e42a49a2a2788e492904dff1686d
JoNee <hhcho1234@naver.com>
2024-05-24 17:38:10 +0900

    마이페이지 회원 정보 수정 제외 모든 구현 기능 -> 진관씨가 회원 정보 수정 구현 예정 회원가입 인증번호 이상하게 작동하던 거 수정

--- Albaya/src/main/java/kh/em/albaya/myPage/model/mapper/MyPageMapper.java
+++ Albaya/src/main/java/kh/em/albaya/myPage/model/mapper/MyPageMapper.java
@@ -10,5 +10,12 @@ public interface MyPageMapper {
 	int myPageCheckPw(Map<String, Object> map);
 
 	String findEncPw(Map<String, Object> map);
-	
+
+	int deleteMember(Map<String, Object> map);
+
+	int updateMemberPw(Map<String, Object> map);
+
+	String findMemberPw(int memberNo);
+
+
 }
