db25915689ccf881d7c37e84175d7735e2a7a8f0
OZ <macoto8@naver.com>
2024-05-27 16:21:48 +0900

    게시글 수정, 댓글

--- Albaya/src/main/java/kh/em/albaya/board/model/mapper/CommentMapper.java
+++ Albaya/src/main/java/kh/em/albaya/board/model/mapper/CommentMapper.java
@@ -1,8 +1,24 @@
 package kh.em.albaya.board.model.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import kh.em.albaya.board.model.dto.Comment;
 
 @Mapper
 public interface CommentMapper {
+
+	// 댓글 조회
+	List<Comment> select(int reviewBoardNo);
+
+	// 댓글 작성
+	int insert(Comment comment);
+
+	// 댓글 수정
+	int update(Comment comment);
+
+	// 댓글 삭제
+	int delete(int commentNo);
 
 }
