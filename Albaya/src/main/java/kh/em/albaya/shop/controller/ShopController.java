05530d6d874d131903d33632788fbb2ac4837c17
JoNee <hhcho1234@naver.com>
2024-05-28 17:46:33 +0900

    기업회원로그인/ 회원가입 페이지 나누기 누락된 파일

--- Albaya/src/main/java/kh/em/albaya/shop/controller/ShopController.java
+++ Albaya/src/main/java/kh/em/albaya/shop/controller/ShopController.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +14,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import kh.em.albaya.member.controller.MemberController;
 import kh.em.albaya.member.model.dto.Member;
 import kh.em.albaya.shop.model.dto.Shop;
@@ -89,5 +92,43 @@ public class ShopController {
             message = "가입 실패";
             return "redirect:/";
          }
+    }
+    
+    @PostMapping("login")
+    public String login(
+    		Shop inputShop,
+    		Model model,
+			@RequestParam(value = "saveId", required = false) String saveId,
+			HttpServletResponse resp,	
+    		RedirectAttributes ra) {
+    	
+		Shop loginShop = service.login(inputShop);
+    	
+		String message = null;
+		
+		if(loginShop == null) {
+			message = "아이디 또는 비밀번호가 일치하지 않습니다";
+			ra.addFlashAttribute("message", message);
+			ra.addFlashAttribute("message", message);
+			return "redirect:/member/login";
+		}
+
+		if(loginShop != null) {
+			model.addAttribute("loginMember", loginShop);
+			
+			Cookie cookie = new Cookie("saveId", loginShop.getShopEmail());
+	
+			cookie.setPath("/");
+			
+			if(saveId != null) {
+				cookie.setMaxAge(30 * 24 * 60 * 60);
+			}
+			else {
+				cookie.setMaxAge(0);
+			}
+			resp.addCookie(cookie);
+			
+		}
+		return "redirect:/";
     }
 }
